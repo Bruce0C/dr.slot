d81a0fee3ebd2925bc5a8491684b653f0216121a
Bruce0C <pvlm5xi2@students.codeinstitute.net>
2025-06-20 12:19:08 +0100

    Update key vars

--- env.py
+++ env.py
@@ -2,3 +2,5 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_7ox0PDFCgBlO@ep-green-art-a2opawlv.eu-central-1.aws.neon.tech/stew_cargo_lived_19658")
+os.environ.setdefault(
+    "SECRET_KEY", "fvinsoejfgblsiaefbvaierabvuer32463gf3rg34")
