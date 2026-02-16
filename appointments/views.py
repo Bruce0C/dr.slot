55f66a548af63447afccdc112ed5f16cf033392f
Bruce0C <pvlm5xi2@students.codeinstitute.net>
2026-02-16 14:18:04 +0000

    Add booking view to handle service selection and day validation

--- appointments/views.py
+++ appointments/views.py
@@ -14,3 +14,29 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def booking(request):
+    # Calling 'validWeekday' Function to Loop days you want in the next 21 days:
+    weekdays = validWeekday(22)
+
+    # Only show the days that are not full:
+    validateWeekdays = isWeekdayValid(weekdays)
+
+    if request.method == 'POST':
+        service = request.POST.get('service')
+        day = request.POST.get('day')
+        if service == None:
+            messages.success(request, "Please Select A Service!")
+            return redirect('booking')
+
+        # Store day and service in django session:
+        request.session['day'] = day
+        request.session['service'] = service
+
+        return redirect('bookingSubmit')
+
+    return render(request, 'booking.html', {
+        'weekdays': weekdays,
+        'validateWeekdays': validateWeekdays,
+    })
