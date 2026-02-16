6998e4d5a10ca9b6bbf785fcea1e282279ca8986
Bruce0C <pvlm5xi2@students.codeinstitute.net>
2026-02-16 16:15:51 +0000

    Update models for availability and appointments

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,4 +1,15 @@
+"""
+This module registers the models from the appointments app
+to the Django admin interface, allowing administrators to
+manage appointments, services, availability, reviews, and notifications.
+
+Admins can perform CRUD operations on these models through the admin panel.
+"""
 from django.contrib import admin
-from .models import *
+from .models import Appointment, Service, Availability
 admin.site.register(Appointment)
 # Register your models here.
+
+admin.site.register(Service)
+admin.site.register(Appointment)
+admin.site.register(Availability)
--- appointments/urls.py
+++ appointments/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from appointments.views import my_appointments
 
 urlpatterns = [
     path('', views.index, name='home'),
