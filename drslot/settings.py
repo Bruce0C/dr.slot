e899dabf689948607f11617499d4bd1d768f5525
Bruce0C <pvlm5xi2@students.codeinstitute.net>
2026-02-18 17:59:23 +0000

    Add Django Allauth for account management and update settings

--- drslot/settings.py
+++ drslot/settings.py
@@ -52,6 +52,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
 
     'appointments',
     'home'
@@ -65,6 +68,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'drslot.urls'
@@ -87,6 +91,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'drslot.wsgi.application'
 
+SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
@@ -129,6 +134,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
