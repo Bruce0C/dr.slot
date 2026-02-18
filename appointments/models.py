4fdf99810214490ba0e02c553aca3f289de4bf4f
Bruce0C <pvlm5xi2@students.codeinstitute.net>
2026-02-18 14:18:00 +0000

    Remove unused service price field and

--- appointments/models.py
+++ appointments/models.py
@@ -28,7 +28,6 @@ class Service(models.Model):
     """
     name = models.CharField(max_length=50, unique=True)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
