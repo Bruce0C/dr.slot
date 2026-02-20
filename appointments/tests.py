ed824ad02231386e1e1eea0657f1103868c19d37
Bruce0C <pvlm5xi2@students.codeinstitute.net>
2026-02-20 04:04:36 +0000

    Add docstring to unit tests for Appointment model

--- appointments/tests.py
+++ appointments/tests.py
@@ -1,3 +1,9 @@
+"""
+This module contains unit tests for the Appointment model
+and related functionality.
+"""
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Service, Appointment
 
 # Create your tests here.
